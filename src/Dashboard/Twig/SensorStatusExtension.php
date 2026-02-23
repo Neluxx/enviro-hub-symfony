9966002a79bd707626a1f0c145ca98c7755de6ca
Neluxx <fabian.arndt96@proton.me>
2026-02-23 20:38:11 +0100

    feat: adjust thresholds for temperature, humidity and carbon dioxide

--- src/Dashboard/Twig/SensorStatusExtension.php
+++ src/Dashboard/Twig/SensorStatusExtension.php
@@ -38,58 +38,58 @@ class SensorStatusExtension extends AbstractExtension
 
     /**
      * Get temperature status class.
-     * Success: 18-24°C
-     * Warning: 16-18°C or 24-26°C
-     * Danger: <16°C or >26°C.
+     * Success: 16-24°C
+     * Warning: 14-16°C or 24-26°C
+     * Danger: < 14°C or > 26°C.
      */
     private function getTemperatureStatus(int $value): string
     {
-        if ($value >= 18 && $value <= 24) {
-            return 'text-bg-success';
+        if ($value < 14 || $value > 26) {
+            return 'text-bg-danger';
         }
 
-        if (($value >= 16 && $value < 18) || ($value > 24 && $value <= 26)) {
+        if ($value < 16 || $value > 24) {
             return 'text-bg-warning';
         }
 
-        return 'text-bg-danger';
+        return 'text-bg-success';
     }
 
     /**
      * Get humidity status class.
      * Success: 40-60%
      * Warning: 30-40% or 60-70%
-     * Danger: <30% or >70%.
+     * Danger: < 30% or > 70%.
      */
     private function getHumidityStatus(int $value): string
     {
-        if ($value >= 40 && $value <= 60) {
-            return 'text-bg-success';
+        if ($value < 30 || $value > 70) {
+            return 'text-bg-danger';
         }
 
-        if (($value >= 30 && $value < 40) || ($value > 60 && $value <= 70)) {
+        if ($value < 40 || $value > 60) {
             return 'text-bg-warning';
         }
 
-        return 'text-bg-danger';
+        return 'text-bg-success';
     }
 
     /**
      * Get CO2 status class.
      * Success: 0-999 ppm
      * Warning: 1000-1999 ppm
-     * Danger: >=2000 ppm.
+     * Danger: >= 2000 ppm.
      */
     private function getCo2Status(int $value): string
     {
-        if ($value >= 0 && $value < 1000) {
-            return 'text-bg-success';
+        if ($value >= 2000) {
+            return 'text-bg-danger';
         }
 
-        if ($value >= 1000 && $value < 2000) {
+        if ($value >= 1000) {
             return 'text-bg-warning';
         }
 
-        return 'text-bg-danger';
+        return 'text-bg-success';
     }
 }
